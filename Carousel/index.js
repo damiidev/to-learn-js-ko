9ed41dd10fc7dcd5778e88ee618d3076a1a5cbb0
damilog <dami.k.dev@gmail.com>
2020-05-07 01:22:28 +0900

    Carousel App Mission
    - 5개 이미지를 순서대로 보여주는 방식
    - 왼쪽, 오른쪽 버튼으로 이미지를 로드함

--- Carousel/index.js
+++ Carousel/index.js
@@ -1,29 +1,42 @@
-const btn = document.querySelector("button");
 const leftBtn = document.querySelector("#leftBtn");
 const rightBtn = document.querySelector("#rightBtn");
-
 const img= document.querySelector("#img");
 
-console.log(img.src);
-btn.addEventListener("click",loadImg());
+rightBtn.addEventListener("click",rihgtImg);
+leftBtn.addEventListener("click",leftImg);
 
-function loadImg(){
-    if(img.src === "image/1.jpg"){
-        document.querySelector("#img").src ="image/2.jpg";
+function rihgtImg(){
+    if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/1.jpg"){
+        img.src="image/2.jpg";
     }
     else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/2.jpg"){
-        document.querySelector("#img").src ="image/3.jpg";
+        img.src ="image/3.jpg";
     }
     else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/3.jpg"){
-        document.querySelector("#img").src ="image/4.jpg";
+        img.src ="image/4.jpg";
     }
-    else if(img.src === "image/4.jpg"){
-        document.querySelector("#img").src ="image/5.jpg";
-    }
-    else if(img.src === "image/5.jpg"){
-        document.querySelector("#img").src ="image/1.jpg";
+    else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/4.jpg"){
+        img.src ="image/5.jpg";
     }
     else{
-        console.log('실패');
+        img.src ="image/1.jpg";
+    }
+}
+
+function leftImg(){
+    if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/1.jpg"){
+        img.src="image/5.jpg";
+    }
+    else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/5.jpg"){
+        img.src ="image/4.jpg";
+    }
+    else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/4.jpg"){
+        img.src ="image/3.jpg";
+    }
+    else if(img.src === "file:///D:/GitHub/to-learn-js-ko/Carousel/image/3.jpg"){
+        img.src ="image/2.jpg";
+    }
+    else{
+        img.src ="image/1.jpg";
     }
 }
