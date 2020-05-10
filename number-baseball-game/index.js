0767a53c92887f85f25cf9024dde3992f446438f
damilog <dami.k.dev@gmail.com>
2020-05-11 01:13:50 +0900

    Feat : 숫자 판별기능, localstorage 저장 기능
    - add 3자리 숫자 판별 기능
    - add localstorage에 입력값 저장하는 기능

--- number-baseball-game/index.js
+++ number-baseball-game/index.js
@@ -1,32 +1,45 @@
 const form = document.querySelector(".js-inputForm"),
  input = form.querySelector("input"),
- inputBtn = document.querySelector(".js-btn");
- inputBtn.addEventListener("click",eventHandler);
+ inputBtn = document.querySelector(".js-btn"),
+ LS_Input ="currentInput";
+
+form.addEventListener("submit",eventHandler);
 let inputValue =[]; // 
 
 const randomNum = makeRandom(100,999), //100~999까지 랜덤 숫자 
  randomNumStr = randomNum.toString(),
  randomNumArray = randomNumStr.split(""); //100~999까지 랜덤 숫자 각 숫자를 array에 삽입함. Ball,Strike 체크용
-console.log(randomNumArray);
 
-function paintInput(text){
-    const li = document.createElement("li");
-    
+ console.log(randomNumArray);
+
+function checkInput(array){ //ball, strike 확인하는 기능
+    if(array[0] !== randomNumArray[0] || array[1] !== randomNumArray[1] || array[2] !== randomNumArray[2]){
+        alert("out");  
+    }
+    else{
+        alert("no");
+    }
+}
+function paintInput(){  //입력한 수, ball strike 개수를 보여주는 기능 
+    const currentInput =localStorage.getItem("currentInput");
+    console.log(currentInput);
 }
 
-function createInputArray(input){
-    const inputNumStr = input.toString(),
-    inputNumArray = inputNumStr.split("");
-    console.log(inputNumArray);
-
+function saveInput(text){ // 입력한 수를 lS 에 저장하는 기능 
+    localStorage.setItem(LS_Input,text);
 }
-
-function eventHandler(event){
+function eventHandler(event){ 
     event.preventDefault(); 
     const inputNum = input.value;
-    createInputArray(inputNum);  
-    paintInput(inputNum);
-
+    inputNumArray = inputNum.split("");  
+    if (inputNumArray.length !== 3){  //3자리인지 판별 
+        alert("3자리 숫자를 입력해주세요.");
+    }
+    else{
+        saveInput(inputNum);
+        checkInput(inputNumArray);
+        paintInput();
+    }   
 }
  function makeRandom(min, max){ //100~999 까지 숫자를 반환하는 함수 
      const result = Math.floor(Math.random()*(max-min+1)) + min;
