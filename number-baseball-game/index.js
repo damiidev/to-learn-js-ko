775197cababf2df75b98ea5af29885edea630113
damilog <dami.k.dev@gmail.com>
2020-05-12 00:07:06 +0900

    Feat : input number의 ball, strike, out 결과 판별 기능
    - ball, strike, out 판별하는 함수 추가
    - checkInput 함수인자로 input  number random number를 받도록 수정

--- number-baseball-game/index.js
+++ number-baseball-game/index.js
@@ -6,29 +6,50 @@ const form = document.querySelector(".js-inputForm"),
 form.addEventListener("submit",eventHandler);
 let inputValue =[]; // 
 
-const randomNum = makeRandom(100,999), //100~999까지 랜덤 숫자 
- randomNumStr = randomNum.toString(),
- randomNumArray = randomNumStr.split(""); //100~999까지 랜덤 숫자 각 숫자를 array에 삽입함. Ball,Strike 체크용
+const randomNum = makeRandom(100,999), //100~999까지 랜덤 숫자 생성
+ randomNumStr = randomNum.toString(), // 랜덤 숫자를 문자열로 변경
+ randomNumArray = randomNumStr.split(""); // 문자열인 랜덤 숫자를 array로 변경
 
  console.log(randomNumArray);
 
-function checkInput(array){ //ball, strike 확인하는 기능
-    if(array[0] !== randomNumArray[0] || array[1] !== randomNumArray[1] || array[2] !== randomNumArray[2]){
-        alert("out");  
+function checkInput(input,random){ //ball, strike 확인하는 기능
+    const inputArray =input,
+    randomArray = random;
+
+    let ballCount = 0,
+     strikeCount = 0,
+     outCount = 0,
+     ballResult ="",
+     strikeResult ="",
+     outResult ="";
+
+    for(i=0;i<=2;i++){
+            if(inputArray[i] === randomArray[i]){
+                strikeCount +=1;
+                strikeResult = `${strikeCount}strike`;
+            }
+            else if(inputArray.includes(randomArray[i])){
+                ballCount +=1;
+                ballResult = `${ballCount}ball`;
+            }
+            else{
+                outCount +=1;
+                outResult = `${outCount}out`;
+            }
+        }
+        return(strikeResult,ballResult,outResult) //strike, ball, out 개수가 담긴 문자열 리턴
     }
-    else{
-        alert("no");
-    }
-}
+
 function paintInput(){  //입력한 수, ball strike 개수를 보여주는 기능 
     const currentInput =localStorage.getItem("currentInput");
     console.log(currentInput);
 }
 
-function saveInput(text){ // 입력한 수를 lS 에 저장하는 기능 
+function saveInput(text){ // 입력한 수를 lS 에 저장하는 함수
     localStorage.setItem(LS_Input,text);
 }
-function eventHandler(event){ 
+
+function eventHandler(event){ //submit시 실행되는 함수 
     event.preventDefault(); 
     const inputNum = input.value;
     inputNumArray = inputNum.split("");  
@@ -37,10 +58,11 @@ function eventHandler(event){
     }
     else{
         saveInput(inputNum);
-        checkInput(inputNumArray);
+        checkInput(inputNumArray,randomNumArray);
         paintInput();
     }   
 }
+
  function makeRandom(min, max){ //100~999 까지 숫자를 반환하는 함수 
      const result = Math.floor(Math.random()*(max-min+1)) + min;
      return result;
