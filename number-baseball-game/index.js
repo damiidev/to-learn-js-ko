91a3987ab6f0f46b8a5ca689de91416e66c8fc75
damilog <dami.k.dev@gmail.com>
2020-05-12 20:11:11 +0900

    Feat : 숫자 야구 게임 완성
    - add 10번 시도시 실패 alert 기능
    - add 시도 횟수를 count 하여 보여주는 기능
    - add ball, strike, out count, 결과값 보여주는 기능
    - add 3strike인 경우 축하 메세지 alert 기능

--- number-baseball-game/index.js
+++ number-baseball-game/index.js
@@ -1,18 +1,35 @@
 const form = document.querySelector(".js-inputForm"),
  input = form.querySelector("input"),
  inputBtn = document.querySelector(".js-btn"),
- LS_Input ="currentInput";
+ count = document.querySelector("#js-count"),
+ result = document.querySelector("#js-result");
 
 form.addEventListener("submit",eventHandler);
-let inputValue =[]; // 
 
 const randomNum = makeRandom(100,999), //100~999까지 랜덤 숫자 생성
  randomNumStr = randomNum.toString(), // 랜덤 숫자를 문자열로 변경
  randomNumArray = randomNumStr.split(""); // 문자열인 랜덤 숫자를 array로 변경
+ console.log(randomNumArray); //정답 숫자 console창에서 확인
 
- console.log(randomNumArray);
+ function makeRandom(min, max){ //100~999 까지 숫자를 반환하는 함수 
+    const result = Math.floor(Math.random()*(max-min+1)) + min;
+    return result;
+}
 
-function checkInput(input,random){ //ball, strike 확인하는 기능
+let clicks = 0;
+
+function countClicks(){ //도전 횟수 체크용 함수
+    if(clicks > 9){  
+        count.innerText=`도전 횟수 : ${clicks}/10`;
+        alert('10번 초과!! 실패!!');
+    }
+    else{
+        clicks +=1;
+        count.innerText=`도전 횟수 : ${clicks}/10`;
+    }
+}
+
+function checkInput(input,random){ //ball, strike, out 확인용 함수
     const inputArray =input,
     randomArray = random;
 
@@ -27,6 +44,7 @@ function checkInput(input,random){ //ball, strike 확인하는 기능
             if(inputArray[i] === randomArray[i]){
                 strikeCount +=1;
                 strikeResult = `${strikeCount}strike`;
+                
             }
             else if(inputArray.includes(randomArray[i])){
                 ballCount +=1;
@@ -36,36 +54,27 @@ function checkInput(input,random){ //ball, strike 확인하는 기능
                 outCount +=1;
                 outResult = `${outCount}out`;
             }
+            result.innerHTML= `${ballResult} ${strikeResult} ${outResult}`; 
+            checkStrike();
         }
-        return(strikeResult,ballResult,outResult) //strike, ball, out 개수가 담긴 문자열 리턴
+         //strike, ball, out 개수가 담긴 문자열 리턴
     }
 
-function paintInput(){  //입력한 수, ball strike 개수를 보여주는 기능 
-    const currentInput =localStorage.getItem("currentInput");
-    console.log(currentInput);
-}
-
-function saveInput(text){ // 입력한 수를 lS 에 저장하는 함수
-    localStorage.setItem(LS_Input,text);
+function checkStrike(){ //3strike 축하용 함수
+    if(result.innerHTML === ' 3strike '){
+        alert("3strike! Congratulation!!!");
+    }
 }
 
 function eventHandler(event){ //submit시 실행되는 함수 
-    event.preventDefault(); 
+    event.preventDefault(); //입력 숫자 유지
     const inputNum = input.value;
     inputNumArray = inputNum.split("");  
     if (inputNumArray.length !== 3){  //3자리인지 판별 
         alert("3자리 숫자를 입력해주세요.");
     }
     else{
-        saveInput(inputNum);
         checkInput(inputNumArray,randomNumArray);
-        paintInput();
+        countClicks();
     }   
 }
-
- function makeRandom(min, max){ //100~999 까지 숫자를 반환하는 함수 
-     const result = Math.floor(Math.random()*(max-min+1)) + min;
-     return result;
- }
-
-
