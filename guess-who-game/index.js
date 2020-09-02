3db626dd8ca5b6d8a87314272a4986a84a72bab9
damilog <dami.k.dev@gmail.com>
2020-09-02 23:53:49 +0900

    Feat : checkAnswer, checkHint Fn
    - checkAnswer 함수. 오답일 경우 마이너스 1점, 0점일 경우 중지하도록 수정
    - checkHint 함수. 힌트 btn 클릭 횟수 카운팅, 힌트 3개 사용시 중지하도록 수정
    - html hint wrap 위치 수정

--- guess-who-game/index.js
+++ guess-who-game/index.js
@@ -4,11 +4,9 @@ const btnHome = document.querySelector(".btn-home"),
  hintWrap = document.querySelector("hintWrap"),
  inputForm = document.querySelector(".js-inputForm"),
  input = inputForm.querySelector("input");
- 
 
 const scoreView = document.querySelector(".score");
-const totalScoreView = document.querySelector(".totalScore");
-
+const totalScoreView = document.querySelector(".totalScore"); 
 const STAGE_LS = "1",
 SCORE_LS = "MY SCORE";
 
@@ -30,46 +28,83 @@ const paintTotalScore = () => {
     totalScoreView.innerText = `총 점수 : ${currentTotalScore}`;
 }
 
-// calTotalScore : LS에 저장된 totalscore을 가져와 새로운 totalscore을 만듦(보완필요)
+//힌트 클릭할 때 마다 다른 힌트 나오도록 작성중(배열에 힌트 넣어서 보여주기?)
+
+const showHint = () => {
+    const hintArray = ["직업 : 가수", "이름 : OOO", "초성 : ㅇㅇㅇ"];
+    countHint();
+}
+
+// checkHint : hint 사용시 2점 감점, 0점일 경우 정답 공개 + 힌트 3개만 사용 가능
+ let score = 10,
+     totalScore = 0,
+     count = 0;
+
+ const checkHint = () => {
+     count += 1;
+     if(count < 3 ) {
+        score -=2;
+        totalScore = score; 
+        saveStageScore(totalScore);
+        paintScore();
+        alert("힌트 사용 : 2점 감점💥");
+     }
+     else if(count === 3) {
+        score -=2;
+        totalScore = score; 
+        saveStageScore(totalScore);
+        paintScore();
+        alert("힌트 사용 : 2점 감점💥");
+     }
+     else {
+         alert("❌❌힌트 3개 사용 완료!!❌❌");
+     }
+ }
+
+// calTotalScore : LS에 저장된 totalscore을 가져와 새로운 totalscore을 만듦
 let currentTotalScore = 0;
 const calTotalScore = () => {
     const currentScore = localStorage.getItem(`Stage ${STAGE_LS}`);
-    currentTotalScore += Number(currnetScore);
+    currentTotalScore += Number(currentScore);
     saveTotalScore(currentTotalScore);
 } 
 
-// checkHint : hint 사용시 2점 감점
- let score = 10,
-  totalScore = 0;
-
- const checkHint = (event) => {
-    alert("힌트 사용 : 2점 감점💥");
-    score -=2;
-    totalScore = score; 
-    saveStageScore(totalScore);
-    paintScore();
- }
-
- // checkAnswer : 오답일 경우 마이너스 1점
+ // checkAnswer : 오답일 경우 마이너스 1점, 0점일 경우 정답 공개 
  const checkAnswer = (event) => {
     event.preventDefault();
     const inputValue = input.value; // 입력된 값
-    
+
     if(inputValue === "아이유"){
         totalScore = score; 
         saveStageScore(totalScore);
         paintScore();
+        calTotalScore();
         alert(`정답입니다!! ${totalScore}점 획득!!😊`);
+        
     }
     else{
-        score -=1;
-        totalScore = score; 
-        saveStageScore(totalScore);
-        paintScore();
-        alert("틀렸습니다!!");
+        if(score > 1){
+            score -=1;
+            totalScore = score; 
+            saveStageScore(totalScore);
+            paintScore();
+            alert("틀렸습니다!!");
+        }
+        else{
+            score -=1;
+            totalScore = score; 
+            saveStageScore(totalScore);
+            paintScore();
+            alert("틀렸습니다!! 0점 입니다!!💥💥");
+        }
     }
 }
 
 // eventHandler
-inputForm.addEventListener("submit",checkAnswer);
-btnHint.addEventListener("click",checkHint); // hint btn 
+const init = () => {
+    saveTotalScore(0);
+    inputForm.addEventListener("submit",checkAnswer);
+    btnHint.addEventListener("click",checkHint); // hint btn 
+}
+
+init();
