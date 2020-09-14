d7f9cd570507319338b65ea5d983aa763ed98bb5
damilog <dami.k.dev@gmail.com>
2020-09-14 18:47:44 +0900

    Feat : timer, checkhint, checkanswer
    - timer : 현재 점수가 0점일 때는 감점 없도록 수정
    - checkHint : 현재 점수가 2점 이하일 경우 힌트 사용 불가하도록 수정
    - checkAnswer :  0점일 경우 점수 차감 없이 정답 공개하도록 수정
    - stage 1,2,3 초기 점수 10점으로 세팅

--- guess-who-game/index.js
+++ guess-who-game/index.js
@@ -21,51 +21,68 @@ clockText = clockContainter.querySelector("h3");
 const SCORE_LS = "SCORE_LS";
 const answer = ["신세경","봉준호","기성용"];
 
+let stage = 1; //Stage 번호
+let startTime = 10; // timer 최초값
 
+let score = 10, //현재 점수
+    realScore = 0,  //현재 점수를 받아서 save 할 때 사용하는 변수
+    initCount = 0, // 최초 힌트 확인 개수 
+    currentTotalScore = 0; // 현재 최종 점수를 계산해서 save 할 때 사용하는 변수
 
-let stage = 1;
-
-let score = 10, //시작 점수
-    realScore = 0, // 
-    initCount = 0;
-let currentTotalScore = 0;
-
-// timer
-let startTime = 30;
+// timer : 10초를 카운트다운하여 10초 경과시 1점씩 감점. 현재 점수가 0점일 때는 감점 없음
 
 const timer = () => {
-    if (startTime > 0){
+    if (startTime > 0){ // 10초~1초 구간일 때 카운트 다운 
         startTime -=1;
         clockText.innerText=`00:${startTime< 10? `0${startTime}` :startTime}`
-    }     
-    else {
-        clearInterval(setTime);
-        alert("시간 초과!! 다음 문제로 이동하세요!!)");
     }      
-}
-const setTime = setInterval(timer,1000); 
+    else {  // 0초일 때 
+        if (score > 1){ // stage score 가 1점 초과일 때 
+            alert("10초 경과 : 1점 감점💥");  
+            score -=1; 
+            saveStageScore(score); 
+            paintScore();
+            paintTotalScore();
+            startTime = 11;  // 타임 리셋
 
-//save.. :stage 점수를 localStorage에 저장하는 함수
+        }
+        else {                  //stage score 가 0점 일 때 
+            alert("10초 경과 : 0점 입니다. 다음 문제로 이동하세요.");
+            saveStageScore(score); 
+            paintScore();
+            paintTotalScore(); 
+            paintHint(initCount);  
+            startTime = 11;  // 타임 리셋
+        }
+    }    
+}
+
+const setTime = setInterval(timer,1000); // timer함수 1초마다 반복
+
+//saveStageScore :현재 stage 점수를 localStorage에 저장하는 함수
 const saveStageScore = (score) => { //😥문제발생) 2,3 stage 값이 10에서 마이너스 된 값이 아닌 1stage 획득 점수 기준에서 마이너스 된 값으로 계산됨. > 해결 
     localStorage.setItem(stage,score);
 } 
 
+//saveTotalScore :전체 stage 점수를 localStorage에 저장하는 함수
 const saveTotalScore = () => {
     const currentScore = localStorage.getItem(stage);
     currentTotalScore += Number(currentScore);
     localStorage.setItem(SCORE_LS,currentTotalScore);
 }
-
+// saveCount : 힌트 확인 개수를 카운트하기 위한 함수 
 const saveCount = (count) => {  
     localStorage.setItem('hint',count);
 }
 
-const initImg= ()=> { 
+// initImg : 초기 이미지를 보여주는 함수
+const initImg = () => { 
     let imgNum = stage;
     imgWrap.appendChild(img);
     img.src = `img/${imgNum}.png`;
 }
 
+// paintImg : 현재 stage를 받아서 stage에 맞는 이미지를 보여주는 함수
 const paintImg = (Stage) => {
     stage +=1 ; 
     let imgNum = stage;
@@ -73,23 +90,23 @@ const paintImg = (Stage) => {
     img.src = `img/${imgNum}.png`;
 }
 
-//paint.. : localStorage에 저장한 stage 점수 보여주는 함수
+//paintScore : localStorage에 저장한 stage 점수 보여주는 함수
 const paintScore = () => {   
     let currentScore = localStorage.getItem(stage);
     scoreView.innerText = `점수 : ${currentScore}점`;
 }
-//✨total 점수 보여주는 부분 업데이트 필요!!!!!!
+
+//paintTotalScore : LoacalStorage에 저장한 Total score을 보여주는 함수 
 const paintTotalScore = () => {   
     let currentTotalScore = localStorage.getItem(SCORE_LS);
     totalScoreView.innerText = `총 점수 : ${currentTotalScore}점`;
 }
 
-//paintHint : 힌트 클릭할 때 마다 순서에 맞게 힌트를 보여주는 함수
+//paintHint : 힌트 클릭할 때 마다 힌트 확인수를 확인하여 현재 순서에 맞는 힌트를 보여주는 함수
 const paintHint = (count) => {
     let getCount = count;
-    console.log(getCount);
 
-    if (stage === 1){
+    if (stage === 1){  // stage 1 힌트 
         const hintArray = ["30대 여성입니다.", "직업은 배우입니다.", "이름 초성은 'ㅅㅅㄱ'입니다."];
         if(getCount === 1){
             hintWrap.innerText = hintArray[0];
@@ -102,7 +119,7 @@ const paintHint = (count) => {
         }   
 
     }
-    else if (stage ===2){
+    else if (stage ===2){ // stage 2 힌트 
         const hintArray = ["50대 남성입니다.", "직업은 영화감독입니다.", "이름 초성은 'ㅂㅈㅎ'입니다."];
         if(getCount === 1){
             hintWrap.innerText = hintArray[0];
@@ -114,9 +131,9 @@ const paintHint = (count) => {
             hintWrap.innerText = hintArray[2];
         }
     }
-    else {
+    else {   // stage 3 힌트 
         const hintArray = ["30대 남성", "직업은 축구선수입니다.", "이름 초성은 'ㄱㅅㅇ'입니다."];
-        if(getCount === 1){
+        if(getCount === 1){ 
             hintWrap.innerText = hintArray[0];
         }
         else if(getCount === 2){
@@ -125,53 +142,35 @@ const paintHint = (count) => {
         else {
             hintWrap.innerText = hintArray[2];
         }
-
-    }
-   
+    }  
 }
 
-// checkHint : hint 사용시 2점 감점, 0점일 경우 정답 공개 + 힌트 3개만 사용 가능, 현재 점수 2점 미만일 경우 힌드 사용 불가
+// checkHint : hint 사용시 2점 감점, 힌트는 3개까지 사용 가능, 현재 점수가 2점 이하일 경우 힌트 사용 불가
 
  const checkHint = () => {
-     //const currentScore = localStorage.getItem(stage); 
      initCount +=1;
-     saveCount(initCount);
-     //let count = localStorage.getItem('hint');
-     
-     if(initCount < 4  ) {
-        score -=2; 
-        saveStageScore(score); 
-        paintScore();
-        paintTotalScore();
-        paintHint(initCount);
-        alert("힌트 사용 : 2점 감점💥");   
+     saveCount(initCount); //hint 사용 횟수를 저장함
+    
+     if(score > 2) {
+        if(initCount < 4  ) {
+            score -=2; 
+            saveStageScore(score); 
+            paintScore();
+            paintTotalScore();
+            paintHint(initCount);
+            alert("힌트 사용 : 2점 감점💥");   
+         }
+         else {
+            saveCount(3);
+            alert("힌트 사용 :힌트 3개 사용 완료!!❌❌");
+         }
      }
-     else {
-        saveCount(3);
-        alert("❌❌힌트 3개 사용 완료!!❌❌");
+     else{
+        alert("힌트 사용 : 현재 점수로는 힌트 사용이 불가합니다.");
      }
  }
-const goNext =() => { 
-    score = 0;
-    reset();
-}
 
-const reset = () => {
-    realScore = score;
-    saveStageScore(realScore);
-    saveTotalScore();
-    paintTotalScore();
-    paintImg(stage);
-    paintScore();
-    initCount = 0;
-    count = 0;
-    saveCount(0);
-    score = 10;
-
-    hintWrap.innerText = '🟢힌트🟢';
-    answerWrap.innerText = '';
-}
-// showAnswer : 정답을 보여주는 함수
+ // showAnswer : 유저가 정답 보기 버튼 클릭시 정답을 보여주는 함수, 현재 점수는 0점으로 변환
 const  showAnswer = () => {
     answerWrap.innerText = `정답 : ${answer[stage-1]}`;
     score = 0;
@@ -180,51 +179,78 @@ const  showAnswer = () => {
     saveTotalScore();
     paintScore();
     paintTotalScore();
-    alert("0점입니다 💥");   
+    alert("정답 보기 : 0점입니다. 다음 문제로 이동하세요.");   
     
 }
+
+// goNext : 다음 문제 버튼 클릭시 호출 되는 함수
+const goNext =() => { 
+    score = 0;
+    reset();
+}
+
+// reset : 모든 값을 리셋하는 함수 (goNext, checkAnswer 함수에서 사용)
+const reset = () => {
+    realScore = score;
+    saveStageScore(realScore);
+    saveTotalScore();
+    paintTotalScore();
+    paintImg(stage);
+    paintScore();
+    initCount = 0; // initcount 리셋
+    count = 0; //count수 리셋
+    saveCount(0);//리셋된 count 수 저장 
+    score = 10; //stage 점수 리셋 
+    hintWrap.innerText = '🟢힌트🟢'; //힌트 영역 리셋
+    answerWrap.innerText = ''; //정답 영역 리셋
+    startTime = 11; // 10초를 리셋
+}
+
  // checkAnswer : 정답을 체크하는 함수. 오답일 경우 마이너스 1점, 0점일 경우 정답 공개 
  const checkAnswer = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //정답 submit 후에 입력된 값 유지
     const inputValue = input.value; // 입력된 값
 
-    if(inputValue === answer[stage-1]){
+    if(inputValue === answer[stage-1]){ // 정답 판단 
         reset();
-        alert(`정답입니다!! ${realScore}점 획득!!😊`);
+        alert(`정답 확인 : 정답입니다!! ${realScore}점 획득!!😊`);
     }
     else{
-        if(score > 1){
+        if(score > 0 ){   //정답이 아니고, stage 점수가 1점 초과 일 경우 
             score -=1;
             realScore = score; 
             saveStageScore(realScore); 
             paintScore();
             paintTotalScore();
-            alert("틀렸습니다!!");
+            alert("정답 확인 : 틀렸습니다!!");
         }
-        else{
-            score -=1;
+        else{    //정답이 아니고, stage 점수가 1점 초과 일 경우 
+            alert("정답 확인 : 0점 입니다💥💥 다음 문제로 이동하세요.");
             realScore = score; 
             saveStageScore(realScore);
             paintScore();
-            paintTotalScore();
-            showAnswer();
-            alert("틀렸습니다!! 0점 입니다!!💥💥");
+            paintTotalScore();           
         }
     }
 }
 
-// eventHandler
+// init: 최초 실행 함수 
 const init = () => {
-    inputForm.addEventListener("submit",checkAnswer);
+    inputForm.addEventListener("submit",checkAnswer); // answer submit 
     btnHint.addEventListener("click",checkHint); // hint btn 
     btnAnswer.addEventListener("click", showAnswer); // answer btn 
     btnNext.addEventListener("click", goNext); // next btn 
-    initImg();
-    saveCount(0);
+
+    initImg(); // 초기 이미지 세팅(1번 이미지)
+    saveCount(0); // 힌트 사용 횟수를 0으로 세팅
+
+    localStorage.setItem(SCORE_LS,0); //Total Score 초기점수
+    localStorage.setItem(1,10); // stage 1 초기 점수
+    localStorage.setItem(2,10); // stage 2 초기 점수
+    localStorage.setItem(3,10); // stage 3 초기 점수
+
     paintTotalScore();
-    localStorage.setItem(SCORE_LS,0);
-    localStorage.setItem(2,10);
-    localStorage.setItem(3,10);
 }
+
 
 init();
